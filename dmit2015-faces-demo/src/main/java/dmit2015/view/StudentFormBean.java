3fb34d5c2bd478f4a593cfcd3ef32e01a6a75202
Kyle Pananganan <smarties2226@gmail.com>
2026-09-11 10:59:05 -0600

    Completed Student Registration demo and added files from today's and last class.

---
+++ dmit2015-faces-demo/src/main/java/dmit2015/view/StudentFormBean.java
@@ -0,0 +1,57 @@
+package dmit2015.view;
+
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+
+import java.io.Serializable;
+
+@Named
+@ViewScoped
+public class StudentFormBean implements Serializable {
+
+    private int submissionCount;
+    private String fullName;
+    private String program;
+    private boolean fullTime;
+
+    public void submit() {
+        submissionCount++;
+        FacesMessage message = new FacesMessage(
+                FacesMessage.SEVERITY_INFO,
+                "Form Submitted",
+                "Welcome " + fullName + " from " + program
+        );
+        FacesContext.getCurrentInstance()
+                .addMessage(null, message);
+
+    }
+    public int getSubmissionCount() {
+        return submissionCount;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public boolean isFullTime() {
+        return fullTime;
+    }
+
+    public void setFullTime(boolean fullTime) {
+        this.fullTime = fullTime;
+    }
+}
